52ed57facf3e7060a7c63abb4a48ab3dd6e8b7e7
SMani24 <50586258+SMani24@users.noreply.github.com>
2023-09-10 00:43:50 +0330

    Added Model + training

--- Downloading the images/DownloadImages.py
+++ Downloading the images/DownloadImages.py
@@ -2,7 +2,7 @@ from simple_image_download import simple_image_download as simp
 
 response = simp.simple_image_download
 
-keyWords = ["orange ping pong ball"]
+keyWords = ["orange ping pong ball", "ping pong ball", "ping pong"]
 
 for kw in keyWords:
-    response().download(kw, 1000) 
+    response().download(kw, 100) 
--- test.py
+++ test.py
@@ -1,9 +1,58 @@
-from ultralytics import YOLO
-from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
+import argparse
+import time
 
-model = YOLO("yolov8m.pt")
+from ultralytics import YOLO
+import supervision as sv
 
-results = model.predict(source="0", show=True)
+def calculateFPS(prevTime):
+    currentTime = time.time()
+    fps = 1 / (currentTime - prevTime)
+    return fps, currentTime
 
-print(reults)
+def parseArguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="YOLOv8 live")
+    parser.add_argument("--webcam-resolution", default=(1280, 720), nargs=2, type=int)
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parseArguments()
+    frameWidth, frameHeight = args.webcam_resolution
+    print(frameWidth, frameHeight)
+
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)
+
+    model = YOLO("yolov8n.pt")
+
+    boxAnnotator = sv.BoxAnnotator(
+        thickness=2,
+        text_thickness=2, 
+        text_scale=1
+    )
+
+    prevTime = time.time()
+    while True:
+        ret, frame = cap.read()
+
+        result = model(frame)[0]
+        detections = sv.Detections.from_yolov8(result)
+
+        frame = boxAnnotator.annotate(scene=frame, detections=detections)
+
+        # Calculate FPS
+        fps, prevTime = calculateFPS(prevTime)
+
+        # Draw FPS on the frame
+        # cv2.putText(frame, f"FPS: {int(fps)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        cv2.imshow("YOLOv8", frame)
+
+        # print(frame.shape)
+        if (cv2.waitKey(30) == 27):
+            break
+
+if __name__ == "__main__":
+    main()
